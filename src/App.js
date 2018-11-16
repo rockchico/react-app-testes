47b8f060bfa9891e68adbdad9171a0ac6e564d19
Francisco Schwertner <rockchico@gmail.com>
2018-11-16 12:21:39 -0200

    testes ciclo de vida

--- src/App.js
+++ src/App.js
@@ -4,20 +4,36 @@ class InputTeste extends React.Component {
   
   constructor(props) {
     super(props);
+    console.log('passou no InputTeste constructor')
   }
 
   dismiss() {
       this.props.unmountMe();
   } 
 
+  componentDidMount() {
+    console.log('passou no InputTeste componentDidMount ');
+  }
+
   componentWillUnmount() {
-    console.log('passou no componentWillUnmount ');
+    console.log('passou no InputTeste componentWillUnmount ');
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('passou no InputTeste componentDidUpdate ');
   }
 
   render(){
+      
+    console.log('passou no InputTeste render ');
+
       // code
       return (
-        <input type='text'></input>
+        <div>
+          { console.log('passou no InputTeste return render ') }
+          <input type='text' onChange={this.props.onChange}></input>
+        </div>
+        
       )
   }
 }
@@ -26,7 +42,7 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    console.log('passou no constructor')
+    console.log('passou no App constructor')
 
     this.state = { counter: 0, renderChild: true };
 
@@ -39,7 +55,7 @@ class App extends React.Component {
   If you need to load data from a remote endpoint, this is a good place to instantiate the network request.
   */
   componentDidMount() {
-    console.log('passou no componentDidMount ');
+    console.log('passou no App componentDidMount ');
   }
 
   
@@ -48,26 +64,27 @@ class App extends React.Component {
     this.setState((prevState, props) => ({ counter: prevState.counter + 1 }))
   }
 
-  _handleChildUnmount(){
-    this.setState({renderChild: false});
+  _handleChildUnmount(render){
+    this.setState({renderChild: render});
   }
 
   // chamado após uma atualização do componente 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('passou no componentDidUpdate ');
+    console.log('passou no App componentDidUpdate ');
   }
 
   render() {
     
-    console.log('passou no render ');
+    console.log('passou no App render ');
 
     const { counter } = this.state;
 
     return (
       <div>
-        { console.log('passou no return render ') }
-        <div><button onClick={this._handleChildUnmount.bind(this)} >x</button></div>
-        {this.state.renderChild ? <InputTeste /> : null}
+        { console.log('passou no App return render ') }
+        <div><button onClick={this._handleChildUnmount.bind(this, false)} >x</button></div>
+        <div><button onClick={this._handleChildUnmount.bind(this, true)} >+</button></div>
+        {this.state.renderChild ? <InputTeste onChange={this._handleChange.bind(this)} /> : null}
         <div>{ counter }</div>
       </div>
     )
